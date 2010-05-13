81ac29d1eff2ff5f5fbdda2a0a7301968f905585
chris.taylor.98 <chris.taylor.98@cb069f5e-2525-11df-97fb-d110b628d502>
2010-05-13 13:07:51 +0000

    Mark recipes as draft version - release tag.

---
+++ tags/release_0023_2010_05_13_02/test/functional/brew_entry_logs_controller_test.rb
@@ -0,0 +1,13 @@
+require File.dirname(__FILE__) + '/../test_helper'
+
+class BrewEntryLogsControllerTest < ActionController::TestCase
+  fixtures :brew_entry_logs, :users
+  def test_observation_edit
+    assert false
+  end
+
+  def test_tasting_edit
+	assert false
+  end
+
+end
