90916e0c5fe1a86f0df5498c4e708eb4bcc6b681
chris.taylor.98 <chris.taylor.98@cb069f5e-2525-11df-97fb-d110b628d502>
2010-09-01 05:59:02 +0000

    Ability for users to add ingredients - release tag.

---
+++ tags/release_0065_2010_09_01/release_0026/test/unit/password_reset_mailer_test.rb
@@ -0,0 +1,15 @@
+require File.dirname(__FILE__) + '/../test_helper'
+
+class PasswordResetMailerTest < ActionMailer::TestCase
+  tests PasswordResetMailer
+  def test_reset
+#TODO: redo test
+#    @expected.subject = 'PasswordResetMailer#reset'
+#    @expected.body    = read_fixture('reset')
+#    @expected.date    = Time.now
+#
+#    assert_equal @expected.encoded, PasswordResetMailer.create_reset(@expected.date).encoded
+	assert false
+  end
+
+end
