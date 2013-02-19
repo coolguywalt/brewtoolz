95b1f5ef1a166df9d0274c9687bcd0693c8f39e7
chris.taylor.98@gmail.com <chris.taylor.98@gmail.com@cb069f5e-2525-11df-97fb-d110b628d502>
2013-02-19 11:48:30 +0000

    Improved brewentry and brewday screens.  Addition of yeast pitch date for cubes - release tag.

---
+++ tags/release_0032_2013_02-19/config/routes.rb
@@ -0,0 +1,83 @@
+ActionController::Routing::Routes.draw do |map|
+
+#    This file is part of Brewtools.
+#
+#    Brewtools is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Lesser General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    Brewtools is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Lesser General Public License for more details.
+#
+#    You should have received a copy of the GNU Lesser General Public License
+#    along with Brewtools.  If not, see <http://www.gnu.org/licenses/>.
+#
+#    Copyright Chris Taylor, 2008, 2009, 2010
+
+
+  map.site_search  'search', :controller => 'front', :action => 'search'
+  map.root :controller => 'front', :action => 'index'
+
+  map.connect '/whatsbrewing', :controller => :brew_entries, :action => :whatsbrewing
+
+  #Shortcut routes for specific tools:
+  map.connect '/tools/refract', :controller => :tools, :action => :index   #Default tool
+  map.connect '/tools/yeast', :controller => :tools, :action => :index, :yeast => 1
+  map.connect '/tools/yeast_pitch', :controller => :tools, :action => :index, :yeast => 1
+  map.connect '/tools/dilution', :controller => :tools, :dilution => 1
+
+  Hobo.add_routes(map)
+
+  # The priority is based upon order of creation: first created -> highest priority.
+
+  # Sample of regular route:
+  #   map.connect 'products/:id', :controller => 'catalog', :action => 'view'
+  # Keep in mind you can assign values other than :controller and :action
+
+  # Sample of named route:
+  #   map.purchase 'products/:id/purchase', :controller => 'catalog', :action => 'purchase'
+  # This route can be invoked with purchase_url(:id => product.id)
+
+  # Sample resource route (maps HTTP verbs to controller actions automatically):
+  #   map.resources :products
+
+  # Sample resource route with options:
+  #   map.resources :products, :member => { :short => :get, :toggle => :post }, :collection => { :sold => :get }
+
+  # Sample resource route with sub-resources:
+  #   map.resources :products, :has_many => [ :comments, :sales ], :has_one => :seller
+  
+  # Sample resource route with more complex sub-resources
+  #   map.resources :products do |products|
+  #     products.resources :comments
+  #     products.resources :sales, :collection => { :recent => :get }
+  #   end
+
+  # Sample resource route within a namespace:
+  #   map.namespace :admin do |admin|
+  #     # Directs /admin/products/* to Admin::ProductsController (app/controllers/admin/products_controller.rb)
+  #     admin.resources :products
+  #   end
+
+  # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
+  # map.root :controller => "welcome"
+
+  # See how all your routes lay out with "rake routes"
+
+ 
+
+
+  # Install the default routes as the lowest priority.
+  # Note: These default routes make all actions in every controller accessible via GET requests. You should
+  # consider removing or commenting them out if you're using named routes and resources.
+
+  map.connect ':controller/:action'
+  map.connect ':controller/:id/:action'
+  map.connect ':controller/:id.:format/:action'
+
+  map.connect ':action', :controller => :front
+
+end
