579f77f09ee4910356371cae79a8e1904ffe38b2
chris.taylor.98 <chris.taylor.98@gmail.com>
2013-02-26 11:59:07 +0000

    Remove old migration file.

--- db/migrate/20130216081447_add_yeast_pitch_date.rb
+++
@@ -1,9 +0,0 @@
-class AddYeastPitchDate < ActiveRecord::Migration
-  def self.up
-    add_column :brew_entries, :yeast_pitched_date, :date
-  end
-
-  def self.down
-    remove_column :brew_entries, :yeast_pitched_date, :date
-  end
-end
