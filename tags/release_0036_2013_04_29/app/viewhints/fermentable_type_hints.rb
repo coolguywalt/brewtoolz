4c44c5cb567714a562e8b79967a105e391f4d0cf
chris.taylor.98@gmail.com <chris.taylor.98@gmail.com@cb069f5e-2525-11df-97fb-d110b628d502>
2013-04-29 12:14:13 +0000

    Updates and improvements to the water calculations - release tag

---
+++ tags/release_0036_2013_04_29/app/viewhints/fermentable_type_hints.rb
@@ -0,0 +1,29 @@
+#    This file is part of Brewtools.
+#
+#    Brewtools is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Lesser General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    Brewtools is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Lesser General Public License for more details.
+#
+#    You should have received a copy of the GNU Lesser General Public License
+#    along with Brewtools.  If not, see <http://www.gnu.org/licenses/>.
+#
+#    Copyright Chris Taylor, 2008, 2009, 2010
+
+class FermentableTypeHints < Hobo::ViewHints
+  field_names :fullyfermentable => "Fully Fermentable"
+
+   field_help :name => "Must be unique",
+       :yeild => "% yeild of the fermentable",
+       :converted => "Is it already converted?",
+       :fullyfermentable => "Is this a simple sugar that is fullly fermentable?",
+       :mashed => "Does this need to be mashed?",
+       :colour => "What is the colour contribution (EBC)",
+       :description => "Provide a brief description",
+       :acidity_type => "Type of grain (base, crystal, roast, acid) for determining acidity contribution"
+end
